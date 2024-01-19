c395f3587b7d01464923a7ae31fdf542fc90334d
MarcoViola88 <150437296+MarcoViola88@users.noreply.github.com>
2024-01-19 17:47:38 +0100

    Update instructions_english.js
    Corrected a typo ("de hypothesis" --> "the hypothesis", line 88)

--- experiment/instructions_english.js
+++ experiment/instructions_english.js
@@ -85,7 +85,7 @@ var text_feedback2_placeholder = "Type here"
 var text_debriefing =
     "<h2>Debriefing</h2>" +
     "<p align='left'>The purpose of this study was actually to study the effect on sexual arousal of <i>believing</i> that the content is AI-generated. " +
-    "Indeed, we want to test de hypothesis that believing that erotic images are fake would lead to a lower emotional arousal. " +
+    "Indeed, we want to test the hypothesis that believing that erotic images are fake would lead to a lower emotional arousal. " +
     "As we are primarily interested in your <i>beliefs</i> about reality, all images were in fact taken from an existing database of real images used in psychology research to study emotions. " +
     "We apologize for the necessary deception used in the instructions, and we hope that you understand its role in ensuring the validity of our experiment.</p>" +
     "<p align='left'><b>Thank you again!</b> Your participation in this study will be kept completely confidential. If you have any questions or concerns about the project, please contact D.Makowski@sussex.ac.uk.</p>" +
