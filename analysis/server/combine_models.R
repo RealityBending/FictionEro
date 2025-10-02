0815d26b32aa1e45aa19a9843c44bb1504199e81
Ana Neves <asf25@sussex.ac.uk>
2025-10-02 11:25:55 +0100

    models

---
+++ analysis/server/combine_models.R
@@ -0,0 +1,40 @@
+library(brms)
+library(cmdstanr)
+library(rstan)
+library(loo)
+
+# Get the number of cores and task ID from the environment variables
+options(mc.cores = as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK")))
+task_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+
+# Folder where your model .rds files are saved
+setwd("/mnt/lustre/users/psych/asf25/fictionero/models/")
+
+# List of models
+model_names <- c("ModelArousal_1", "ModelEnticement_1", "ModelValence_1",
+                 "ModelArousal_2", "ModelEnticement_2", "ModelValence_2")
+
+# Pick model based on array ID
+model_name <- model_names[task_id]
+
+combine_and_save <- function(name) {
+  print(paste0("Starting: ", name, " at ", Sys.time()))
+  
+  # Find all files for this model (e.g., ModelArousal_1_1.rds, ModelArousal_1_2.rds, …)
+  files <- list.files(".", pattern = paste0("^", name, "_.*\\.rds$"), full.names = TRUE)
+  
+  if (length(files) == 0) stop("No .rds files found for ", name)
+  
+  # Combine model objects
+  m <- brms::combine_models(mlist = lapply(files, readRDS))
+  
+  # Add WAIC criterion
+  m <- brms::add_criterion(m, "waic", ndraws = 1500, file = name)
+  
+  # Save combined model
+  saveRDS(m, paste0(name, "_combined.rds"))
+  
+  print(paste0("Finished: ", name, " at ", Sys.time()))
+}
+
+combine_and_save(model_name)
--- analysis/server/make_model.R
+++ analysis/server/make_model.R
@@ -1,17 +1,24 @@
 # Fit models
 
-library(tidyverse)
-library(easystats)
-library(patchwork)
-library(ggside)
-library(glmmTMB)
 library(brms)
+library(cmdstanr)
+library(tidyverse)
 
-options(mc.cores = parallel::detectCores(),
-        brms.backend = "cmdstanr",
-        width = 300)
+task_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID", unset = "1"))
+chains_per_task <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK", unset = "2"))
+start_chain <- (task_id - 1) * chains_per_task + 1
+iter <- 4000 
+warmup <- iter / 2
+seed = 1234 + start_chain
 
-# Study 1 ===================================================================================================================================
+
+path <- "/mnt/lustre/users/psych/asf25/fictionero/"
+models_dir <- file.path(path, "models")
+dir.create(models_dir, recursive = TRUE, showWarnings = FALSE)
+
+# ----------------------------
+# Study 1
+# ----------------------------
 
 dfsub1 <- dfsub <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/refs/heads/main/study1/data/data_participants.csv")
 df1 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/refs/heads/main/study1/data/data.csv") |> 
@@ -39,31 +46,58 @@ df1 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/ref
          ConditionBelief = fct_relevel(ConditionBelief, "True", "False")) |> 
   mutate(across(starts_with("Feedback_"), function(x) {fct_relevel(x, "False", "True")}))
 
-df1[1:2,] # keep only 2 participants to test locally 
 
-# MODELS --------
+# ----------------------------
+# MODELS - Study 1
+# ----------------------------
 
 # Arousal
+f_a1 <- brms::brmsformula(Arousal ~ Gender / Relevance/ Condition*ConditionBelief + (1|Participant) + (1|Item))
 
-# random slopes that did not converge: (Condition:Relevance/ Participant) + 
+m_a1 <-  brms::brm(
+  formula = f_a1,
+  data = df1, 
+  family = zero_one_inflated_beta(),
+  chains = chains_per_task,   
+  cores = chains_per_task,    
+  iter = iter, 
+  warmup = warmup,
+  seed = 1234 + start_chain,
+  backend = "cmdstanr",
+  file = file.path(models_dir, paste0("ModelArousal_1_task_", task_id))
+)
 
+# Enticement (FIXED: use f_e1 and set chains)
+f_e1 <- brms::brmsformula(Enticement ~ Gender / Relevance/ Condition*ConditionBelief + (1|Participant) + (1|Item))
 
-m_a1 <-  brms::brm(Arousal ~ Gender / Relevance/ Condition*ConditionBelief + (1|Participant) + (1|Item),
-                          data=df1, family=zero_one_inflated_beta())
-
-# Enticement
-m_e1 <-  brms::brm(Enticement ~ Gender /Relevance/Condition*ConditionBelief + (1|Participant) + (1|Item),
-                          data=df1, family=zero_one_inflated_beta())
-
+m_e1 <-  brms::brm(
+  formula = f_e1,
+  data = df1, 
+  family = zero_one_inflated_beta(),
+  chains = chains_per_task,   
+  cores = chains_per_task,    
+  iter = iter, 
+  warmup = warmup,
+  seed = 1234 + start_chain,
+  backend = "cmdstanr",
+  file = file.path(models_dir, paste0("ModelEnticement_1_task_", task_id))
+)
 
 # Valence
-m_v1 <-  brms::brm(Valence ~ Gender / Relevance/ Condition*ConditionBelief + (1|Participant) + (1|Item),
-                          data=df1, family=zero_one_inflated_beta())
+f_v1 <- brms::brmsformula(Valence ~ Gender / Relevance/ Condition*ConditionBelief + (1|Participant) + (1|Item))
 
-# Save models
-saveRDS(m_a1, file = "../models/ModelArousal_1.rds")
-saveRDS(m_e1, file = "../models/ModelEnticement_1.rds")
-saveRDS(m_v1, file = "../models/ModelValence_1.rds")
+m_v1 <-  brms::brm(
+  formula = f_v1,
+  data = df1, 
+  family = zero_one_inflated_beta(),
+  chains = chains_per_task,   
+  cores = chains_per_task,    
+  iter = iter, 
+  warmup = warmup,
+  seed = 1234 + start_chain,
+  backend = "cmdstanr",
+  file = file.path(models_dir, paste0("ModelValence_1_task_", task_id))
+)
 
 
 # Study 2 ----------------------------------------------------------------------
@@ -103,23 +137,54 @@ df2 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/ref
     TRUE ~ "Individual")) |>
   mutate(StimuliType = fct_relevel(StimuliType, "Individual", "Couple"))
 
-df2[1:2,] # keep only 2 participants to test locally 
+# ----------------------------
+# MODELS - Study 2
+# ----------------------------
 
-
-# MODELS --------
 # Arousal
-m_a2<-  brms::brm(Arousal ~ Gender / Condition*ConditionBelief + (Condition|Participant) + (1|Item),
-                          data=df2, family=zero_one_inflated_beta())
+f_a2 <- brms::brmsformula(Arousal ~ Gender / Condition*ConditionBelief + (1|Participant) + (1|Item))
+
+m_a2 <-  brms::brm(
+  formula = f_a2,
+  data = df2, 
+  family = zero_one_inflated_beta(),
+  chains = chains_per_task,   
+  cores = chains_per_task,    
+  iter = iter, 
+  warmup = warmup,
+  seed = 1234 + start_chain,
+  backend = "cmdstanr",
+  file = file.path(models_dir, paste0("ModelArousal_2_task_", task_id))
+)
+
 # Enticement
-m_e2 <-  brms::brm(Enticing ~ Gender / Condition*ConditionBelief + (1|Participant) + (1|Item),
-                          data=df2,family=zero_one_inflated_beta())
+f_e2 <- brms::brmsformula(Enticing ~ Gender / Condition*ConditionBelief + (1|Participant) + (1|Item))
+
+m_e2 <-  brms::brm(
+  formula = f_e2,
+  data = df2, 
+  family = zero_one_inflated_beta(),
+  chains = chains_per_task,   
+  cores = chains_per_task,    
+  iter = iter, 
+  warmup = warmup,
+  seed = 1234 + start_chain,
+  backend = "cmdstanr",
+  file = file.path(models_dir, paste0("ModelEnticement_2_task_", task_id))
+)
+
 # Valence
-m_v2 <-  brms::brm(Valence ~ Gender / Condition*ConditionBelief + (1|Participant) + (1|Item),
-                          data=df2,family=zero_one_inflated_beta())
+f_v2 <- brms::brmsformula(Valence ~ Gender / Condition*ConditionBelief + (1|Participant) + (1|Item))
 
-# Save models
-saveRDS(m_a2, file = "../models/ModelArousal_2.rds")
-saveRDS(m_e2, file = "../models/ModelEnticement_2.rds")
-saveRDS(m_v2, file = "../models/ModelValence_2.rds")
-
-
+m_v2 <-  brms::brm(
+  formula = f_v2,
+  data = df2, 
+  family = zero_one_inflated_beta(),
+  chains = chains_per_task,   
+  cores = chains_per_task,    
+  iter = iter, 
+  warmup = warmup,
+  seed = 1234 + start_chain,
+  backend = "cmdstanr",
+  file = file.path(models_dir, paste0("ModelValence_2_task_", task_id))
+)
