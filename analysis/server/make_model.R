2d0749de9ce419307408a21e0685256644a3cc1e
Ana Neves <147059054+AnafNeves@users.noreply.github.com>
2025-10-10 09:37:31 +0100

    Update make_model.R

--- analysis/server/make_model.R
+++ analysis/server/make_model.R
@@ -20,32 +20,7 @@ dir.create(models_dir, recursive = TRUE, showWarnings = FALSE)
 # Study 1
 # ----------------------------
 
-dfsub <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/refs/heads/main/study1/data/data_participants.csv")
-df1 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/refs/heads/main/study1/data/data.csv") |>
-  right_join(
-    select(dfsub, Participant, Sample, Language, Mobile, starts_with("Feedback_"),
-           BAIT_Visual, BAIT_Text, AI_Knowledge, SexualActivity,
-           GAAIS_Negative, GAAIS_Positive, Porn, COPS_Frequency_2,
-           Screen_Size,
-           -Feedback_Comments),
-    by = "Participant"
-  ) |>
-  rename(Gender = Sex) |>
-  datawizard::rescale(select=c("Valence"), range=c(-1, 1), to=c(0, 1)) |>
-  mutate(Condition = fct_relevel(Condition, "Photograph", "AIGenerated"),
-         Relevance =  fct_relevel(Relevance, "Relevant", "Irrelevant", "NonErotic"),
-         Gender =  fct_relevel(Gender, "Male", "Female"),
-         PornFrequency = as.numeric(as.factor(COPS_Frequency_2)),
-         SexualActivity_num = as.numeric(as.factor(SexualActivity)),
-         ConditionBelief = case_when(
-           Condition == "Photograph" & Realness > 0.5 ~ "True",
-           Condition == "AIGenerated" & Realness < 0.5 ~ "True",
-           .default = "False"
-         ),
-         ConditionBelief = as.factor(ConditionBelief),
-         ConditionBelief = fct_relevel(ConditionBelief, "True", "False")) |>
-  mutate(across(starts_with("Feedback_"), function(x) {fct_relevel(x, "False", "True")}))
-
+df1 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/refs/heads/main/analysis/data/df1.csv")
 
 # ----------------------------
 # MODELS - Study 1
@@ -102,40 +77,8 @@ m_v1 <-  brms::brm(
 
 # Study 2 ----------------------------------------------------------------------
 
-dfsub2 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/refs/heads/main/study2/data/data_participants.csv")
-df2 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/refs/heads/main/study2/data/data.csv") |>
-  right_join(
-    select(dfsub2, Participant, Mobile, starts_with(c("Feedback_","BAIT")), COPS_Frequency, SexualActivity,- Feedback_Text),
-    by = "Participant"
-  ) |>
-  datawizard::rescale(select= c("Arousal", "Enticing", "Valence"), range=c(0, 6), to=c(0,1)) |>
-  datawizard::rescale(select= c("Realness"), range=c(-3,3), to=c(0,1)) |>
-  mutate(Condition = case_when(
-    Condition == "Fiction" ~ "AI-Generated",
-    Condition == "Reality" ~ "Photograph"
-  )) |>
-  mutate(Condition = fct_relevel(Condition, "Photograph", "AIGenerated"),
-         Gender =  fct_relevel(Gender, "Male", "Female"),
-         SexualOrientation = fct_relevel(SexualOrientation, "Heterosexual", "Homosexual", "Bisexual"),
-         PornFrequency = as.numeric(as.factor(COPS_Frequency)),
-         SexualActivity_num = as.numeric(as.factor(SexualActivity)),
-         ConditionBelief = case_when(
-           Condition == "Photograph" & Realness > 0.5 ~ "True",
-           Condition == "AIGenerated" & Realness < 0.5 ~ "True",
-           .default = "False"),
-         ConditionBelief = as.factor(ConditionBelief),
-         ConditionBelief = fct_relevel(ConditionBelief, "True", "False")) |>
-  rename(AllRealConfidence = "Feedback_AllRealConfidence",
-         AllFakeConfidence = "Feedback_AllFakeConfidence",
-         Enjoyment = "Feedback_Enjoyment",
-         Item = Stimulus
-  )|>
-  mutate(across(starts_with("Feedback_"), as.factor)) |>
-  mutate(ConditionBelief = as.factor(ConditionBelief)) |>
-  mutate(StimuliType = case_when(
-    grepl("couple", Item, ignore.case = TRUE) ~ "Couple",
-    TRUE ~ "Individual")) |>
-  mutate(StimuliType = fct_relevel(StimuliType, "Individual", "Couple"))
+df2 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/refs/heads/main/analysis/data/df2.csv")
+
 
 # ----------------------------
 # MODELS - Study 2
@@ -158,7 +101,7 @@ m_a2 <-  brms::brm(
 )
 
 # Enticement
-f_e2 <- brms::brmsformula(Enticing ~ Gender / Condition*ConditionBelief + (1|Participant) + (1|Item))
+f_e2 <- brms::brmsformula(Enticement ~ Gender / Condition*ConditionBelief + (1|Participant) + (1|Item))
 
 m_e2 <-  brms::brm(
   formula = f_e2,
