5e07a4eb3080672b3bb7c4113507f79137b1d31c
Ana Neves <asf25@sussex.ac.uk>
2025-12-09 11:39:43 +0000

    Update cleaned_data.R

--- analysis/server/cleaned_data.R
+++ analysis/server/cleaned_data.R
@@ -15,22 +15,6 @@ df1 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/ref
   ) |>
   rename(Gender = Sex) |>
   datawizard::rescale(select=c("Valence"), range=c(-1, 1), to=c(0, 1)) |>
-  
-
-# |>
-#   mutate(Condition = fct_relevel(Condition, "Photograph", "AIGenerated"),
-#          Relevance =  fct_relevel(Relevance, "Relevant", "Irrelevant", "NonErotic"),
-#          Gender =  fct_relevel(Gender, "Male", "Female"),
-#          PornFrequency = as.numeric(as.factor(COPS_Frequency_2)),
-#          SexualActivity_num = as.numeric(as.factor(SexualActivity)),
-#          ConditionBelief = case_when(
-#            Condition == "Photograph" & Realness > 0.5 ~ "True",
-#            Condition == "AIGenerated" & Realness < 0.5 ~ "True",
-#            .default = "False"
-#          ),
-#          ConditionBelief = as.factor(ConditionBelief),
-#          ConditionBelief = fct_relevel(ConditionBelief, "True", "False")) |>
-#   mutate(across(starts_with("Feedback_"), function(x) {fct_relevel(x, "False", "True")}))
 
 
 write.csv(df1, "../data/df1.csv", row.names = FALSE)
@@ -54,27 +38,8 @@ df2 <- read.csv("https://raw.githubusercontent.com/RealityBending/FictionEro/ref
                Condition == "Photograph" & Realness > 0.5 ~ "True", Condition
                == "AIGenerated" & Realness < 0.5 ~ "True", .default =
                "False"))
-  
-      
-
-# |>
-#   mutate(Condition = fct_relevel(Condition, "Photograph", "AIGenerated"),
-#          Gender =  fct_relevel(Gender, "Male", "Female"),
-#          SexualOrientation = fct_relevel(SexualOrientation, "Heterosexual", "Homosexual", "Bisexual"),
-#          PornFrequency = as.numeric(as.factor(COPS_Frequency)),
-#          SexualActivity_num = as.numeric(as.factor(SexualActivity)),
-#          ConditionBelief = as.factor(ConditionBelief),
-#          ConditionBelief = fct_relevel(ConditionBelief, "True", "False")) |>
-#   mutate(across(starts_with("Feedback_"), as.factor)) |>
-#   mutate(ConditionBelief = as.factor(ConditionBelief)) |>
-#   mutate(StimuliType = case_when(
-#     grepl("couple", Item, ignore.case = TRUE) ~ "Couple",
-#     TRUE ~ "Individual")) |>
-#   mutate(StimuliType = fct_relevel(StimuliType, "Individual", "Couple")) |>
-
 
 write.csv(df2, "../data/df2.csv", row.names = FALSE)
-
 
 
 ############## DATA VIsUALISATION
